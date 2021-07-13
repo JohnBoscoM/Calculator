e7f624b2b4a045df93238f255a3689bf2d77ce1f
JohnBoscoM <john.matanda@outlook.com>
2021-07-13 16:49:18 +0200

    Created Calculator object with addition and subtraction method

---
+++ src/Calculator/Calculator.java
@@ -0,0 +1,14 @@
+package Calculator;
+
+public class Calculator {
+    Calculator(){}
+
+    public double addition(){
+        return 0;
+    }
+
+    public double subtraction(){
+        return 0;
+    }
+
+}
